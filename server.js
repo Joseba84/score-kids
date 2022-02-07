9f8da192a7284cf8cd63562bc260a17be94c02a2
Joseba Bello <joseba.bello@gluckgames.com>
2022-02-07 18:05:05 +0100

    fix app name in server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/score-kids'));
+app.use(express.static(__dirname + '/dist/score-kids-1'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/score-kids/index.html'));});
+'/dist/score-kids-1/index.html'));});
 app.listen(process.env.PORT || 8080);
